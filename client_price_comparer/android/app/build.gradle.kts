e60d8aa9beff901fbed802cb55106376c21c6d15
Pierre Laclaverie <pierre.laclaverie@gmail.com>
2025-06-03 21:17:15 -0400

    edit android config file to be at newer version of android NDK

--- client_price_comparer/android/app/build.gradle.kts
+++ client_price_comparer/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.client_price_comparer"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // Compatibility issue
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
