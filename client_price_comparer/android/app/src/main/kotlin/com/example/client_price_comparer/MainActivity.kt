dbe7e0c6b73cb3d44232d8a0fae42fd5511054b9
Pierre Laclaverie <pierre.laclaverie@gmail.com>
2025-06-10 19:38:26 -0400

    remove the old project to fully transition to monorepos

--- client_price_comparer/android/app/src/main/kotlin/com/example/client_price_comparer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.client_price_comparer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- client_price_comparer/lib/models/price/price_models.dart
+++
@@ -1,5 +0,0 @@
-enum ProductDisplayMode
-{
-  minimal,
-  advanced,
-}
