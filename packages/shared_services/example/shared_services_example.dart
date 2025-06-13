4c1bd301b304ee6e75e8eb4c2be5285119b96c0e
Pierre Laclaverie <pierre.laclaverie@gmail.com>
2025-06-12 20:32:03 -0400

    add simple service in app and empty shared_services

---
+++ packages/shared_services/example/shared_services_example.dart
@@ -0,0 +1,6 @@
+import 'package:shared_services/shared_services.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/shared_services/test/shared_services_test.dart
@@ -0,0 +1,16 @@
+import 'package:shared_services/shared_services.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
